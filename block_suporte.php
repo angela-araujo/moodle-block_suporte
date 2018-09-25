011119bbf7655962f52999b551dd7b044b322245
Ronnald R Machado <rrmachado@id.uff.br>
2018-09-25 13:23:39 -0300

    CCE restriction - added

--- block_suporte.php
+++ block_suporte.php
@@ -4,7 +4,7 @@ class block_suporte extends block_base {
         $this->title = get_string('suporte', 'block_suporte');
     }
 	public function get_content() {
-	global $USER;
+	global $USER, $COURSE;
     if ($this->content !== null) {
       return $this->content;
     }
@@ -13,11 +13,16 @@ class block_suporte extends block_base {
 	$email= $USER->email;
 	$matricula=$USER->username;
     $this->content         =  new stdClass;
-    $this->content->text   = '<div class="text-center" ><p style="font-size:0.9em">Para suporte técnico no uso do ambiente, clique abaixo.</p><form action="http://atendimento.ccead.puc-rio.br/index.php?a=add" method="POST" target="_blank">
-		<input type="hidden" id="moodle_fullname" name="moodle_fullname" value="' . $nome . " " . $sobrenome .'"/>
-		<input type="hidden" id="moodle_email" name="moodle_email" value="'. $email .'"/>
-		<input type="hidden" id="moodle_username" name="moodle_username" value="'. $matricula .'"/>
-		<input class="btn btn-defaut btn-xs" type="submit" value="Atendimento"/></form></div>';;
+    if($COURSE->category==56){
+        $this->content->text='';
+    }else{
+        $this->content->text   = '<div class="text-center" ><p style="font-size:0.9em">Para suporte técnico no uso do ambiente, clique abaixo.</p><form action="http://atendimento.ccead.puc-rio.br/index.php?a=add" method="POST" target="_blank">
+        <input type="hidden" id="moodle_fullname" name="moodle_fullname" value="' . $nome . " " . $sobrenome .'"/>
+        <input type="hidden" id="moodle_email" name="moodle_email" value="'. $email .'"/>
+        <input type="hidden" id="moodle_username" name="moodle_username" value="'. $matricula .'"/>
+        <input class="btn btn-defaut btn-xs" type="submit" value="Atendimento"/></form></div>';
+    }
+    
     $this->content->footer = '';
  
     return $this->content;
